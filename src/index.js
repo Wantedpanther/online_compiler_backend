31d719554f259373164140442af81a10bd475992
john <johntehnician@gmail.com>
2023-08-24 22:28:33 +0900

    c++-version1

--- src/index.js
+++ src/index.js
@@ -1,47 +1,41 @@
-const {runCode} = require("./run-code");
-const {supportedLanguages} = require("./run-code/instructions");
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express');
+const { exec } = require('child_process');
 const cors = require("cors");
-const {info} = require("./run-code/info");
+const corsOpts = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+};
+const app = express();
+const port = 3000;
+app.use(cors(corsOpts));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+app.use(express.json());
 
-const sendResponse = (res, statusCode, body) => {
-    const timeStamp = Date.now()
+app.post('/', (req, res) => {
+  const code = req.body.code;
 
-    res.status(statusCode).send({
-        timeStamp,
-        status: statusCode,
-        ...body
-    })
-}
-
-app.post("/", async (req, res) => {
-    try {
-        const output = await runCode(req.body)
-        sendResponse(res, 200, output)
-    } catch (err) {
-        sendResponse(res, err?.status || 500, err)
+  exec(`g++ -o compiled_code -xc++ -`, (error, stdout, stderr) => {
+    if (error) {
+      res.send(stderr);
+    } else {
+      res.send(stdout);
     }
-})
-
+  }).stdin.end(code);
+});
 app.get('/list', async (req, res) => {
     const body = []
 
-    for(const language of supportedLanguages) {
-        body.push({
-            language,
-            info: await info(language),
-        })
-    }
+    // for(const language of supportedLanguages) {
+    //     body.push({
+    //         language,
+    //         // info: await info(language),
+    //     })
+    // }
 
-    sendResponse(res, 200, {supportedLanguages: body})
+    // sendResponse(res, 200, {supportedLanguages: body})
 })
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
